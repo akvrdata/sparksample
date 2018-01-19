d6da5dfc8b038923eb7faeb0afe6cdba3a672152
akvrdata <34913074+akvrdata@users.noreply.github.com>
2018-01-19 21:13:57 +0530

    sam

--- src/main/scala/spark_sample.scala
+++ src/main/scala/spark_sample.scala
@@ -3,14 +3,17 @@ import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext._
 
-  val conf = new SparkConf().setAppName("SparkSample")
-  val sc = new SparkContext(conf);
-  sc.setLogLevel("WARN")
+ 
 
 object spark_sample {
 
   def main(args:Array[String])
   {
+
+ val conf = new SparkConf().setAppName("SparkSample")
+  val sc = new SparkContext(conf);
+  sc.setLogLevel("WARN")
+
     val l = List("arun","vishnu","swathi","sachin","aman","pratap")
     val rdd = l.map(x=>(x,1))
 
