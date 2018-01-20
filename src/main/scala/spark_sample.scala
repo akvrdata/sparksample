326535ce2ad7c381766c3b83ee6f0638b0f35ff9
akvrdata <34913074+akvrdata@users.noreply.github.com>
2018-01-20 11:40:42 +0530

    sdf

--- src/main/scala/spark_sample.scala
+++ src/main/scala/spark_sample.scala
@@ -10,7 +10,7 @@ object spark_sample {
   def main(args:Array[String])
   {
 
-  val conf = new SparkConf().setAppName("spark_sample")
+  val conf = new SparkConf().setAppName("spark_sample").setMaster("local[2]").set("spark.executor.memory","1g");
   val sc = new SparkContext(conf)
   sc.setLogLevel("WARN")
 
